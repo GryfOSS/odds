360609aa1e364a1834e4dbfec519d0897706194c
Bartosz Pachołek <bartosz+github@idct.tech>
2025-08-19 00:26:04 +0200

    coverage tests

--- tests/OddsFactoryTest.php
+++ tests/OddsFactoryTest.php
@@ -147,4 +147,138 @@ class OddsFactoryTest extends TestCase
         $this->assertEquals('-111.11', $odds->getMoneyline());
         $this->assertEquals('52.63', $odds->getProbability());
     }
+
+    public function testFromDecimalEdgeCases(): void
+    {
+        // Test minimum valid decimal
+        $odds = $this->factory->fromDecimal('1.00');
+        $this->assertEquals('1.00', $odds->getDecimal());
+        $this->assertEquals('0/1', $odds->getFractional());
+        $this->assertEquals('0', $odds->getMoneyline());
+        $this->assertEquals('100.00', $odds->getProbability());
+
+        // Test decimal with many digits (should be rounded)
+        $odds = $this->factory->fromDecimal('2.005');
+        $this->assertEquals('2.01', $odds->getDecimal());
+    }
+
+    public function testFromDecimalNonNumeric(): void
+    {
+        $this->expectException(InvalidPriceException::class);
+        $this->expectExceptionMessage('Invalid decimal value provided: abc. Min value: 1.0');
+
+        $this->factory->fromDecimal('abc');
+    }
+
+    public function testFromDecimalNegative(): void
+    {
+        $this->expectException(InvalidPriceException::class);
+        $this->expectExceptionMessage('Invalid decimal value provided: -2.0. Min value: 1.0');
+
+        $this->factory->fromDecimal('-2.0');
+    }
+
+    public function testFromFractionalZeroNumerator(): void
+    {
+        $odds = $this->factory->fromFractional(0, 1);
+        $this->assertEquals('1.00', $odds->getDecimal());
+        $this->assertEquals('0/1', $odds->getFractional());
+        $this->assertEquals('0', $odds->getMoneyline());
+        $this->assertEquals('100.00', $odds->getProbability());
+    }
+
+    public function testFromFractionalLargeNumbers(): void
+    {
+        $odds = $this->factory->fromFractional(100, 1);
+        $this->assertEquals('101.00', $odds->getDecimal());
+        $this->assertEquals('100/1', $odds->getFractional());
+        $this->assertEquals('+10000', $odds->getMoneyline());
+    }
+
+    public function testFromFractionalInvalidNegativeDenominator(): void
+    {
+        $this->expectException(InvalidPriceException::class);
+        $this->expectExceptionMessage('Invalid denominator provided');
+
+        $this->factory->fromFractional(1, -1);
+    }
+
+    public function testFromMoneylineNonNumeric(): void
+    {
+        $this->expectException(InvalidPriceException::class);
+        $this->expectExceptionMessage('Invalid moneyline value provided: abc');
+
+        $this->factory->fromMoneyline('abc');
+    }
+
+    public function testFromMoneylinePositiveWithoutSign(): void
+    {
+        $odds = $this->factory->fromMoneyline('100');
+        $this->assertEquals('2.00', $odds->getDecimal());
+        $this->assertEquals('+100', $odds->getMoneyline());
+    }
+
+    public function testFromMoneylinePositiveWithSign(): void
+    {
+        $odds = $this->factory->fromMoneyline('+150');
+        $this->assertEquals('2.50', $odds->getDecimal());
+        $this->assertEquals('+150', $odds->getMoneyline());
+    }
+
+    public function testFromMoneylineVeryHighPositive(): void
+    {
+        $odds = $this->factory->fromMoneyline('10000');
+        $this->assertEquals('101.00', $odds->getDecimal());
+        $this->assertEquals('+10000', $odds->getMoneyline());
+    }
+
+    public function testFromMoneylineVeryHighNegative(): void
+    {
+        $odds = $this->factory->fromMoneyline('-10000');
+        $this->assertEquals('1.01', $odds->getDecimal());
+        $this->assertEquals('-10000', $odds->getMoneyline());
+    }
+
+    public function testFromMoneylineDecimalValues(): void
+    {
+        $odds = $this->factory->fromMoneyline('150.50');
+        $this->assertEquals('2.51', $odds->getDecimal());
+        $this->assertEquals('+150.50', $odds->getMoneyline());
+    }
+
+    public function testDefaultConversionComplexFractional(): void
+    {
+        // Test default fractional conversion with tolerance
+        $factory = new OddsFactory(); // No odds ladder
+
+        $odds = $factory->fromDecimal('3.33');
+        $this->assertEquals('3.33', $odds->getDecimal());
+        // Should use continued fractions algorithm
+        $this->assertNotEmpty($odds->getFractional());
+    }
+
+    public function testMoneylineFormatting(): void
+    {
+        // Test moneyline formatting for whole numbers
+        $odds = $this->factory->fromDecimal('3.00');
+        $this->assertEquals('+200', $odds->getMoneyline()); // Should not have decimals
+
+        $odds = $this->factory->fromDecimal('1.50');
+        $this->assertEquals('-200', $odds->getMoneyline()); // Should not have decimals
+    }
+
+    public function testDefaultFractionalConversionEdgeCases(): void
+    {
+        $factory = new OddsFactory(); // No odds ladder
+
+        // Test continued fractions algorithm edge cases
+        $odds = $factory->fromDecimal('1.33');
+        $this->assertEquals('33/100', $odds->getFractional());
+
+        $odds = $factory->fromDecimal('1.67');
+        $this->assertEquals('67/100', $odds->getFractional());
+
+        $odds = $factory->fromDecimal('2.33');
+        $this->assertEquals('133/100', $odds->getFractional());
+    }
 }
--- tests/OddsTest.php
+++ tests/OddsTest.php
@@ -48,4 +48,88 @@ class OddsTest extends TestCase
         $this->assertEquals('1/1', $odds->getFractional());
         $this->assertEquals('1/1', $odds->getFractional());
     }
+
+    public function testDecimalNormalization(): void
+    {
+        // Test that decimal values are normalized to 2 decimal places
+        $odds = new Odds('2', '1/1', '+100');
+        $this->assertEquals('2.00', $odds->getDecimal());
+
+        $odds = new Odds('2.5', '3/2', '+150');
+        $this->assertEquals('2.50', $odds->getDecimal());
+
+        $odds = new Odds('1.123', '1/8', '-800');
+        $this->assertEquals('1.12', $odds->getDecimal());
+    }
+
+    public function testInvalidDecimalNonNumeric(): void
+    {
+        $this->expectException(InvalidPriceException::class);
+        $this->expectExceptionMessage('Invalid decimal value provided: abc. Min value: 1.0');
+
+        new Odds('abc', '1/1', '+100');
+    }
+
+    public function testInvalidDecimalZero(): void
+    {
+        $this->expectException(InvalidPriceException::class);
+        $this->expectExceptionMessage('Invalid decimal value provided: 0. Min value: 1.0');
+
+        new Odds('0', '1/1', '+100');
+    }
+
+    public function testInvalidDecimalNegative(): void
+    {
+        $this->expectException(InvalidPriceException::class);
+        $this->expectExceptionMessage('Invalid decimal value provided: -1.5. Min value: 1.0');
+
+        new Odds('-1.5', '1/1', '+100');
+    }
+
+    public function testMinimumValidDecimal(): void
+    {
+        $odds = new Odds('1.0', '0/1', '0');
+        $this->assertEquals('1.00', $odds->getDecimal());
+        $this->assertEquals('100.00', $odds->getProbability());
+    }
+
+    public function testHighDecimalOdds(): void
+    {
+        $odds = new Odds('100.0', '99/1', '+9900');
+        $this->assertEquals('100.00', $odds->getDecimal());
+        $this->assertEquals('1.00', $odds->getProbability());
+    }
+
+    public function testProbabilityCalculationEdgeCases(): void
+    {
+        // Test probability calculation for edge cases
+        $odds = new Odds('1.01', '1/100', '-10000');
+        $this->assertEquals('99.01', $odds->getProbability());
+
+        $odds = new Odds('10.00', '9/1', '+900');
+        $this->assertEquals('10.00', $odds->getProbability());
+
+        $odds = new Odds('1.5', '1/2', '-200');
+        $this->assertEquals('66.67', $odds->getProbability());
+    }
+
+    public function testDecimalRounding(): void
+    {
+        // Test that decimal values are properly rounded
+        $odds = new Odds('2.005', '1/1', '+100');
+        $this->assertEquals('2.01', $odds->getDecimal()); // Should round up
+
+        $odds = new Odds('2.004', '1/1', '+100');
+        $this->assertEquals('2.00', $odds->getDecimal()); // Should round down
+    }
+
+    public function testAllGettersReturnStrings(): void
+    {
+        $odds = new Odds('2.00', '1/1', '+100');
+
+        $this->assertIsString($odds->getDecimal());
+        $this->assertIsString($odds->getFractional());
+        $this->assertIsString($odds->getMoneyline());
+        $this->assertIsString($odds->getProbability());
+    }
 }
